3ff0024ecc67bf9156965ba1355f905db0847768
XYL0neer <xyl0neer@proton.me>
2025-08-05 16:30:02 +0200

    warn user if using a list in matrix

--- src/components/MatrixForm.vue
+++ src/components/MatrixForm.vue
@@ -14,7 +14,19 @@ import { oneDark } from '@codemirror/theme-one-dark'
 const lintYaml = linter((view) => {
   const diagnostics: Diagnostic[] = []
   try {
-    load(view.state.doc.toString())
+    const text = view.state.doc.toString()
+    const yaml = load(text)
+    const { strategy } = strategySchema.parse(yaml)
+    if (Array.isArray(strategy.matrix)) {
+      const matrixStart = text.indexOf('matrix')
+      diagnostics.push({
+        from: matrixStart,
+        to: matrixStart + 6,
+        message:
+          'Defined a list in matrix which is not a valid matrix definition. Consider to put the list under `strategy.matrix.include`',
+        severity: 'warning',
+      })
+    }
   } catch (error) {
     if (error instanceof YAMLException) {
       console.log(error)
@@ -49,7 +61,11 @@ const strategySchema = z.object({
 
 const formSchema = z.string().refine((val) => {
   try {
-    load(val)
+    const yaml = load(val)
+    const { strategy } = strategySchema.parse(yaml)
+    if (Array.isArray(strategy.matrix)) {
+      return false
+    }
     return true
   } catch (error) {
     console.log(error)
